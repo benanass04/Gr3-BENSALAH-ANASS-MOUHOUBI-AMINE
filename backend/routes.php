4ff810009226eabc6df185bd7ad57601dbceaf7d
Anass Ben Salah <benanass04@gmail.com>
2025-02-14 23:31:31 -0500

    final comit yes

--- backend/routes.php
+++ backend/routes.php
@@ -40,7 +40,10 @@
         ActivitiesController::addActivity();
     });
 
-    
+    // Pour retourner les activites filtrers (GET /api/activities/filter?coach=1&level=3&location=1)
+    get('/api/activities/filter', function(){
+        ActivitiesController::getFiltredActivities($_GET);
+    });
 
     // route par défaut pour les erreurs 404
     any('/404', function() {
